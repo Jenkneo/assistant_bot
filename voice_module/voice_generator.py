fb6dabc6ba52d4cc608fc5f9eee56e2ed943497c
Evgeny <jenkneo.lal@gmail.com>
2023-06-07 04:44:52 +0400

    Update readme

--- voice_module/voice_generator.py
+++ voice_module/voice_generator.py
@@ -14,44 +14,43 @@ PUT_ACCENT = True
 PUT_YO = True
 device = torch.device('cpu') #cpu, cuda, xpu, mkldnn, opengl, opencl, ideep, hip, ve, ort, mlc, xla, lazy, vulkan, meta, hpu
 
+
 class Speaker:
     def __init__(self):
-        self.model, _ =  torch.hub.load(repo_or_dir='snakers4/silero-models',
-                                   model = 'silero_tts',
-                                   language = LANGUAGE,
-                                   speaker = MODEL_ID)
+        self.model, _ = torch.hub.load(
+            repo_or_dir='snakers4/silero-models',
+            model='silero_tts',
+            language=LANGUAGE,
+            speaker=MODEL_ID)
 
         self.model.to(device)
 
     def say(self, text):
-        self.audio = self.model.apply_tts(text = text,
-                                speaker = SPEAKER,
-                                sample_rate = SAMPLE_RATE,
-                                put_accent = PUT_ACCENT,
-                                put_yo = PUT_YO)
+        self.audio = self.model.apply_tts(
+            text=text,
+            speaker=SPEAKER,
+            sample_rate=SAMPLE_RATE,
+            put_accent=PUT_ACCENT,
+            put_yo=PUT_YO)
 
         sd.play(self.audio, SAMPLE_RATE * 1.05)
         time.sleep((len(self.audio)/SAMPLE_RATE) + 0.5)
         sd.stop()
 
+
 if __name__ == "__main__":
+    # Примеры голосов
     spek = Speaker()
-    SPEAKER = 'baya'
-    text = "Привет, это Байя!"
-    spek.say(text);
-    print(text)
 
-    SPEAKER = 'kseniya';
-    text = "Привет, это Ксения!"
-    spek.say(text);
-    print(text)
+    characters = [
+        ['baya', 'Байя'],
+        ['kseniya', 'Ксения'],
+        ['xenia', 'Ксения'],
+        ['aidar', 'Айдар']
+    ]
 
-    SPEAKER = 'xenia';
-    text = "Привет, это тоже Ксения!"
-    spek.say(text);
-    print(text)
-
-    SPEAKER = 'aidar';
-    text = "Привет, а это я, Айдар!"
-    spek.say(text);
-    print(text)
+    for character in characters:
+        SPEAKER = character[0]
+        text = f'Привет, это {character[1]} !'
+        spek.say(text)
+        print(text)
